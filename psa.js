d3c2c262c3dac91d81d10cda29497db6251d4524
HvWiz <89791672+HvWiz@users.noreply.github.com>
2023-04-11 16:13:46 -0400

    Add files via upload

---
+++ psa.js
@@ -0,0 +1,43 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    img = document.querySelector('#img1')
+    vid = document.querySelector('#vid1')
+    observer = new IntersectionObserver(function(entries) {
+        if(entries[0].isIntersecting === true)
+            element = entries[0].target
+            element.classList.add('fade')
+    }, { threshold: [0] });
+    observer.observe(vid);
+    observer.observe(img);
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    img.style.width = `${window.innerWidth - 100}px`
+    vid.style.width = `${window.innerWidth - 100}px`
+    scrolls = document.querySelectorAll('.scroll')
+    scrollDomain = document.querySelector('#scrollDomain')
+    navBar = document.querySelector('#nav')
+    sticky = nav.offsetTop;
+    document.addEventListener('scroll', function() {
+        winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        scrollPercent = (winScroll / height) * 100;
+        scroller.style.width = `${scrollPercent}%`
+        scrolls[1].style.top = `${navBar.offsetHeight}px`
+        scrolls[2].style.top = `${navBar.offsetHeight}px`
+        if (window.pageYOffset > sticky) {
+            for (s of scrolls) {
+                s.classList.add("sticky");
+            }
+          } else {
+            for (s of scrolls) {
+                s.classList.remove("sticky");
+            }
+        }
+        scrollDomain.style.visibility = 'visible'
+        if (document.documentElement.scrollTop == 0) {
+            scrollDomain.style.visibility = 'hidden'
+        }
+    })
+    window.addEventListener('resize', () => {
+        img.style.width = `${window.innerWidth - 100}px`
+        vid.style.width = `${window.innerWidth - 100}px`
+    })
+})
