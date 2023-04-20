fc922616bf2af7111356ce5a5cb2169e214627f6
HvWiz <89791672+HvWiz@users.noreply.github.com>
2023-04-20 19:15:04 -0400

    Update psa.js

--- psa.js
+++ psa.js
@@ -35,8 +35,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
         scroller.style.width = `${scrollPercent}%`
         scrolls[1].style.top = `${navBar.offsetHeight}px`
         scrolls[2].style.top = `${navBar.offsetHeight}px`
-        b.style.width = '230px'
         b.style.top = '87%'
+        b.style.width = '50px'
         if (window.pageYOffset > sticky) {
             for (s of scrolls) {
                 s.classList.add("sticky");
